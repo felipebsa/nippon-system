46a43d20a5eb5d0996f38aa6ae38ca3e996b50b2
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-06-29 14:34:55 -0300

    feat: add model and schemas from clients

--- backend/schemas/client.py
+++ backend/schemas/client.py
@@ -1,4 +1,41 @@
 from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+class SchemaClientCreate(BaseModel):
+    name: str
+    cpf: str
+    cep: Optional[str]
+    address: Optional[str]
+    email: Optional[str]
+    tel: Optional[str]
 
-class SchemaClient(BaseModel):
-    pass
+class SchemaClientUpdate(BaseModel):
+    name: Optional[str]
+    cpf: Optional[str]
+    cep: Optional[str]
+    address: Optional[str]
+    email: Optional[str]
+    tel: Optional[str]
+    expired:  Optional[bool]
+
+class SchemaClientAddress(BaseModel):
+    cep: Optional[str]
+    address: Optional[str]
+
+class SchemaClientContact(BaseModel):
+    email: Optional[str]
+    tel: Optional[str]
+
+class SchemaExpired(BaseModel):
+    expired: bool
+
+class SchemaClientResponse(BaseModel):
+    client_id: int
+    name: str
+    cpf: str
+    cep: Optional[str]
+    address: Optional[str]
+    email: Optional[str]
+    tel: Optional[str]
+    expired:  bool
+    created: datetime
