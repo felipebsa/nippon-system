ed52cc4907e248e54f6189d7c16c477bf67fb321
felipessantosbsa-cypher <felipessantosbsa@gmail.com>
2026-07-08 17:36:51 -0300

    fix: envolved database_url in os.getenv

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-engine = create_engine("DATABASE_URL")
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 class Base(DeclarativeBase):
 	pass
